f005ef34ac52c086f4bd281a6362a494195c556a
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-01-22 22:15:24 +0530

    Update palindrome.py
    Simple loop added

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,21 @@
+# Simple Loop
+
+def is_palindrome(s):
+       temp_list = []
+    for char in s:
+        if char.isalnum():         # 1. Letter illa Number-ah irundha mattum
+            lower_char = char.lower() # 2. Adha chinna ezhutha mathu
+            temp_list.append(lower_char) # 3. List-la sethuko
+    
+    clean_s = "".join(temp_list)   # 4. List-la irukkura ellathayum ottu
+    return clean_s == clean_s[::-1]
+# Example
+print(is_palindrome("A man, a plan, a canal: Panama")) # True
+print(is_palindrome("race a car"))                    # False
+
+
+
+
 #list comprehensions or Generator Expression
 def is_palindrome(s):
     # Step 1: Filter alphanumeric and convert to lowercase
