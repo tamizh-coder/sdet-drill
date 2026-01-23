9a8ea9900418bfe18a6f3ab7f2b046ef611cde10
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-01-23 21:49:13 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -15,4 +15,31 @@ public class StringPalindrome {
         else
             System.out.println("Not a Palindrome");
     }
+
+
+# java code for palindrome number
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reverse)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
 }
