a1e8bc5b34aafb6b5a74cd9f6d65186f3f4183fd
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-01-27 22:41:27 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,20 @@
 #Print numbers 1-100. For multiples of 3 print 'Fizz', multiples of 5 print 'Buzz', multiples of both print 'FizzBuzz'.
+def fizz_buzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Entry point
+if __name__ == "__main__":
+    fizz_buzz()
+
+#Get range from end user
 
 def fizz_buzz(n):
     for i in range(1, n + 1):
