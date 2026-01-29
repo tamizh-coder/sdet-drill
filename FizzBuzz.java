4007a363b617475d93e04a2bb3e7088d1e1da938
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-01-29 22:09:40 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -15,3 +15,35 @@ public class FizzBuzz {
         }
     }
 }
+
+// get input from user
+
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        fizzBuzz(n);
+        sc.close();
+    }
+}
