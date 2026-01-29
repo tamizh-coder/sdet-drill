0b6b7e8a34a8fa5845d48e127ffd9c57a6147319
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-01-29 22:07:15 +0530

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,1 +1,17 @@
 // Print numbers 1-100. For multiples of 3 print 'Fizz', multiples of 5 print 'Buzz', multiples of both print 'FizzBuzz'.
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
