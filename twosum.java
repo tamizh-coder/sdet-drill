0324ffdd88a21b9b268acdfbfd60fbf0cd743737
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-02-03 21:36:34 +0530

    Add TwoSum class with input and output functionality
    Implement TwoSum algorithm with user input handling.

--- twosum.java
+++ twosum.java
@@ -1,1 +1,47 @@
 #twosum
+
+
+  import java.util.*;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int required = target - nums[i];
+
+            if (map.containsKey(required)) {
+                return new int[] { map.get(required), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target value: ");
+        int target = sc.nextInt();
+
+        int[] result = twoSum(nums, target);
+
+        if (result.length > 0) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum solution found");
+        }
+
+        sc.close();
+    }
+}
