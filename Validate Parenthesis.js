393e7c4ec3421469ac3287dc76f5e9e58f97fe3b
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-02-04 15:05:47 +0530

    Add function to validate parentheses
    Implement function to validate parentheses in a string.

--- Validate Parenthesis.js
+++ Validate Parenthesis.js
@@ -1,1 +1,28 @@
 //validate parathesis
+
+function isValid(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (const char of s) {
+        // If closing bracket
+        if (char in map) {
+            // Pop and compare
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        } 
+        // Opening bracket
+        else {
+            stack.push(char);
+        }
+    }
+
+    // Stack must be empty at the end
+    return stack.length === 0;
+}
+
