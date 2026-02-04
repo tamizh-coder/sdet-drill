e3c6b3cc300733ef041da68e079fe335b343f565
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-02-04 14:48:42 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -5,7 +5,7 @@
 
 public class TwoSum {
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) { 
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
