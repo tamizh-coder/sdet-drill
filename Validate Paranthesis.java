8fa3afab8006126a55bff934e9ba8742392c9334
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-02-07 18:32:43 +0530

    Add ValidParentheses class with isValid method
    Implement a method to validate parentheses using a stack.

--- Validate Paranthesis.java
+++ Validate Paranthesis.java
@@ -1,1 +1,37 @@
 //validate paranthesis
+import java.util.*;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for (char ch : s.toCharArray()) {
+            // Closing bracket
+            if (map.containsKey(ch)) {
+                if (stack.isEmpty() || stack.pop() != map.get(ch)) {
+                    return false;
+                }
+            } 
+            // Opening bracket
+            else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter brackets string: ");
+        String s = sc.nextLine();
+
+        System.out.println(isValid(s));
+        sc.close();
+    }
+}
