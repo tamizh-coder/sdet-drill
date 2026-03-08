1c4a52332b3fa2ecfbb9630046060b43bad7d9fb
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-03-08 14:10:26 +0530

    Correct FizzBuzz condition for multiples of 5
    Fix logical error in FizzBuzz condition for multiples of 5.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,7 +3,7 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 3 == 0 && i % 5 == 00) {
                 System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
