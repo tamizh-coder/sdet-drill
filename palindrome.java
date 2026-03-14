835b9ff139be6c3b714d21417ceaa2973db5c4f0
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-03-14 22:37:35 +0530

    Fix typo in prompt for user input

--- palindrome.java
+++ palindrome.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StringPalindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a strig: ");
         String text = sc.nextLine();
 
         String reverse = new StringBuilder(text).reverse().toString();
