4f4beb182fa7f65ffc4ba9a80efa2905f2584add
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-03-29 22:13:20 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 00) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if (i % 3  0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
