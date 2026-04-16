2eed2996cbae62a856c854a47bce133f68650bb0
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-04-16 22:26:44 +0530

    Rename parameter 't' to 'target' in twoSum method

--- TwoSum.java
+++ TwoSum.java
@@ -5,7 +5,7 @@
 
 public class TwoSum {
 
-    public static int[] twoSum(int[] nums, int t) { 
+    public static int[] twoSum(int[] nums, int target) { 
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
